4123fde9fe122051094ea50023db343d83598327
Koharudin <koha001>
2026-05-25 06:43:57 +0700

    adjust

--- be/app/Models/Habits.php
+++ be/app/Models/Habits.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 class Habits extends Model
@@ -27,5 +29,34 @@ class Habits extends Model
     public function objIndikators()
     {
         return $this->hasMany(HabitIndikator::class, "habit_id", "id")->orderBy("order", "asc");
+    }
+
+    public static function generate(){
+        $habits = Habits::with(["objIndikators"])->get();
+        $students = Student::all();
+        $date = Carbon::createFromDate(2026,6,1);
+        $dates = Collection::times(
+            $date->daysInMonth,
+            fn ($day) => $date->copy()->day($day)
+        );
+        
+        $habits->each(function($habit) use($students,$dates){
+            $habit->objIndikators()->each(function($indicator) use($students,$dates){
+                $students->each(function($student) use($dates,$indicator){
+                    $dates->each(function($d) use($student,$indicator){
+                        $activity = new StudentActivity();
+                        $activity->indicator_id = $indicator->id;
+                        $activity->student_id = $student->id;
+                        $activity->tgl = $d;
+                        $activity->skor = "0";
+                        $activity->save();
+                    });
+                });
+            });
+            
+        });
+
+        echo "done";
+        
     }
 }
---
+++ be/app/Models/StudentActivity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+class StudentActivity extends Model
+{
+    //
+    public $table = "trx_activity";
+
+}
