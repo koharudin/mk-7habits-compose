dc303d6d1bdd86b8641481b6dd6b05fb2c988ada
Koharudin <koharudin.mail07@gmail.com>
2026-05-27 07:29:18 +0000

    Fix

--- fe/full-version/next.config.ts
+++ fe/full-version/next.config.ts
@@ -4,12 +4,7 @@ const nextConfig: NextConfig = {
   basePath: process.env.BASEPATH,
   redirects: async () => {
     return [
-      {
-        source: '/',
-        destination: '/:lang/dashboards/crm',
-        permanent: true,
-        locale: false
-      },
+     
       {
         source: '/:lang(en|fr|ar)',
         destination: '/:lang/dashboards/crm',
